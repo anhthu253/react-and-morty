979657902e3fb3eaf423aa9f0e5982374fbdea9b
Thu Nguyen <thu_nguyen@gmx.de>
2022-10-02 15:25:07 +0200

    preserve favorite stage

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ function App() {
   const [characters, setCharacters] = useState(
     JSON.parse(localStorage.getItem("characters")) ?? []
   );
+  const [favorites, setFavorites] = useState(
+    JSON.parse(localStorage.getItem("favorites")) ?? []
+  );
 
   async function fetchCharacters() {
     const response = await fetch(rootURL);
@@ -27,15 +30,17 @@ function App() {
       };
     });
 
-    setCharacters(() =>
-      profiles.map((profile) => {
-        return {
-          id: Math.random().toString(36).substring(2),
-          profile: profile,
-          detail: false,
-          favorite: false,
-        };
-      })
+    setCharacters((characters) =>
+      characters.lenght === 0
+        ? profiles.map((profile) => {
+            return {
+              id: Math.random().toString(36).substring(2),
+              profile: profile,
+              detail: false,
+              favorite: false,
+            };
+          })
+        : characters
     );
   }
 
@@ -66,19 +71,23 @@ function App() {
       )
     );
   }
-  console.log(characters);
+
   useEffect(() => {
     fetchCharacters();
   }, []);
 
   useEffect(() => {
     localStorage.setItem("characters", JSON.stringify(characters));
+    setFavorites(characters.filter((character) => character.favorite));
   }, [characters]);
+
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
 
   return (
     <div className="App">
       <Header>Rick and Morty</Header>
-      {/* <Cards /> */}
       <Routes>
         <Route
           path="/"
@@ -114,7 +123,7 @@ function App() {
           path="/favorites"
           element={
             <Cards
-              characters={characters.filter((character) => character.favorite)}
+              characters={favorites}
               onMoreDetails={(id, index) => toggleDetails(id)}
               showMore="unknown"
               markFavorite="unknown"
